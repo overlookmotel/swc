beb8efa64bbd027486f037881028e0a9fe389b0d
overlookmotel <theoverlookmotel@gmail.com>
2022-04-10 16:27:27 +0100

    Deserializer: Patterns

--- deserialize/test.js
+++ deserialize/test.js
@@ -631,6 +631,140 @@ describe('Parses correctly', () => {
 		]);
 	});
 
+	describe('Patterns', () => {
+		itParses('Binding identifiers', [
+			'let x;',
+			'let var_name_longer_than_7_chars;'
+		]);
+
+		describe('Array patterns', () => {
+			itParses('simple', [
+				'[] = a',
+				'[x] = a',
+				'[x, y, z] = a',
+				'[, , x, , , y, , , z, , ,] = a'
+			]);
+
+			itParses('Rest elements', [
+				'[...x] = a',
+				'[x, y, ...z] = a',
+				'[, , ...z] = a',
+				'[, , x, , , y, , , ...z] = a'
+			]);
+
+			itParses('Assignment patterns', [
+				'[x = 1] = a',
+				'[x = 1, y = 2, z = 3] = a',
+				'[, , x = 1] = a',
+				'[, , x = 1, , , y = 2, , , z = 3, , ,] = a',
+				'[, , x = 1, , , y = 2, , , ...z] = a'
+			]);
+
+			itParses('Expression patterns', [
+				'[x.b] = a',
+				'[x.b.c.d] = a',
+				'[x.b = 1, y.c = 2, z.d = 3] = a',
+				'[, , x.b = 1] = a',
+				'[, , x.b = 1, , , y.c = 2, , , z.d = 3, , ,] = a',
+				'[, , x.b = 1, , , y.c = 2, , , ...z.d] = a'
+			]);
+		});
+
+		describe('Object patterns', () => {
+			itParses('empty', [
+				'({} = a)'
+			]);
+
+			itParses('Shortcuts', [
+				'({x} = a)',
+				'({x, y, z} = a)'
+			]);
+
+			itParses('Key value patterns', [
+				'({b: x} = a)',
+				'({b: x, c: y, d: z} = a)',
+				"({'x x': x} = a)",
+				"({'x x': x, 'y y': y, 'z z': z} = a)",
+				'({1: x} = a)',
+				'({1: x, 2: y, 3: z} = a)',
+				// TODO Tests for BigInt keys
+				'({ [xx]: x } = a)',
+				'({ [xx]: x, [yy]: y, [zz]: z} = a)',
+				'({ [1]: x } = a)',
+				'({ [1]: x, [2]: y, [3]: z} = a)'
+			]);
+
+			itParses('Assignment pattern properties', [
+				'({x = xx} = a)',
+				'({x = xx, y = yy, z = zz} = a)',
+				'({x = 1} = a)',
+				'({x = 1, y = 2, z = 3} = a)'
+			]);
+
+			itParses('Rest elements', [
+				'({...x} = a)',
+				'({x, y, ...z} = a)',
+				'({b: x, c: y, ...z} = a)',
+				'({x = 1, y = 2, ...z} = a)'
+			]);
+
+			itParses('Assignment patterns', [
+				'({b: x = 1} = a)',
+				'({b: x = 1, c: y = 2, d: z = 3} = a)',
+				"({'x x': x = 1} = a)",
+				"({'x x': x = 1, 'y y': y = 2, 'z z': z = 3} = a)",
+				'({1: x = 2} = a)',
+				'({1: x = 2, 3: y = 4, 5: z = 6} = a)',
+				// TODO Tests for BigInt keys
+				'({ [xx]: x = 1 } = a)',
+				'({ [xx]: x = 1, [yy]: y = 2, [zz]: z = 3 } = a)',
+				'({ [1]: x = 2 } = a)',
+				'({ [1]: x = 2, [3]: y = 4, [5]: z = 6} = a)',
+				'({b: x = 1, c: y = 2, ...z} = a)'
+			]);
+
+			itParses('Expression patterns', [
+				'({b: x.c} = a)',
+				'({b: x.c.d.e} = a)',
+				'({b: x.c, d: y.e, f: z.g} = a)',
+				'({b: x.c, d: y.e, ...z.g} = a)',
+				"({'x x': x.b} = a)",
+				"({'x x': x.b, 'y y': y.c, 'z z': z.d} = a)",
+				// TODO Tests for BigInt keys
+				'({ [xx]: x.b } = a)',
+				'({ [xx]: x.b, [yy]: y.c, [zz]: z.d} = a)',
+				'({ [1]: x.b } = a)',
+				'({ [1]: x.b, [2]: y.c, [3]: z.d} = a)'
+			]);
+		});
+
+		itParses('combination of patterns', [
+			`[
+				{
+					b,
+					c: d,
+					e: [
+						, ,
+						{f, g: h.i = 1},
+						,
+						...{j, k: l.m.n, o: p = 2}
+					],
+					...{q, r: s, ...t}
+				},
+				[u],
+				[
+					v,
+					, ,
+					w = 3,
+					, ,
+					{x: {y}},
+					, ,
+					...z
+				]
+			] = a`
+		]);
+	});
+
 	describe('Functions', () => {
 		itParses('Function declarations', [
 			'function f() {}',
@@ -770,6 +904,7 @@ function conformSpans(ast) {
 			} else if (
 				(key === 'await' && node.type === 'ForOfStatement')
 				|| (key === 'questionDotToken' && node.type === 'OptionalChainingExpression')
+				|| (key === 'rest' && node.type === 'RestElement')
 				|| (
 					key === 'spread'
 					&& (
@@ -781,6 +916,7 @@ function conformSpans(ast) {
 				// Special cases for:
 				// - `ForOfStatement` which has an additional span under `await` key
 				// - `OptionalChainingExpression` which has an additional span under `questionDotToken` key
+				// - `RestElement` which has an additional span under `rest` key
 				// - `SpreadElement` which has an additional span under `spread` key
 				// - `ExpressionOrSpread` which may have a span under `spread` key
 				child.start -= offset;

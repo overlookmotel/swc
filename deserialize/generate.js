088ad400c443aebc54212f74845dd9c4dad5f643
overlookmotel <theoverlookmotel@gmail.com>
2022-04-18 21:09:01 +0100

    Relocate codegen code [refactor]

--- deserialize/generate.js
+++ deserialize/generate.js
@@ -5,12 +5,66 @@ const { writeFileSync } = require('fs'),
     pathJoin = require('path').join;
 
 // Imports
-const { init, generateDeserializer } = require('./kinds.js'),
-    types = require('./types/index.js'),
+const { types, initType } = require('./types/index.js'),
     utils = require('./utils.js');
 
 // Generate deserializer code
 
-init(types);
-const code = generateDeserializer(utils);
-writeFileSync(pathJoin(__dirname, 'index.js'), code);
+const DEBUG = !!process.env.DEBUG;
+
+initType('Program');
+writeFileSync(pathJoin(__dirname, 'index.js'), generateDeserializer());
+
+/**
+ * Generate code for deserializer.
+ * @returns {string} - Code for deserializer
+ */
+function generateDeserializer() {
+    let code = [
+        '// Generated code. Do not edit.',
+        "'use strict';",
+        "module.exports = deserialize;",
+        'let buff, int32, uint32;',
+        removeIndent(`function deserialize(buffIn) {
+            const { buffer } = buffIn;
+            buff = Buffer.from(buffer);
+            int32 = new Int32Array(buffer);
+            uint32 = new Uint32Array(buffer);
+            return deserializeProgram(buffIn.byteOffset + buffIn.length - ${types.Program.length});
+        }`),
+        ''
+    ].join('\n\n');
+
+    for (const type of Object.values(types)) {
+        let deserializerCode = removeIndent(type.generateDeserializer());
+        if (DEBUG) {
+            deserializerCode = deserializerCode.replace(
+                /function deserialize.+\n/,
+                line => line + `\tdebugBuff('${type.name}', buff, pos, ${type.length});\n`
+            );
+        }
+        code += deserializerCode + '\n\n';
+    }
+
+    for (const utilName of ['deserializeOption', 'deserializeBox', 'deserializeVec', 'getPtr']) {
+        code += utils[utilName].toString() + '\n\n';
+    }
+
+    if (DEBUG) code += utils.debugBuff.toString() + '\n\n';
+
+    return code.slice(0, -1);
+}
+
+/**
+ * Remove indentation from function code.
+ * Expects to receive a code for a function with opening on first line.
+ * @param {string} code 
+ * @returns {string} - Code with indentation removed
+ */
+function removeIndent(code) {
+    const lines = code.split('\n');
+    if (lines.length === 1) return code;
+
+    const indentDepth = lines[1].match(/^\s+/)[0].length - 4;
+    return [lines[0], ...lines.slice(1).map(line => line.slice(indentDepth))].join('\n');
+}
--- deserialize/types/index.js
+++ deserialize/types/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// Modules
+const assert = require("assert");
+
+// Exports
+const types = {};
+module.exports = { types, initType, getTypeName };
+
 // Imports
 const programTypes = require('./program.js'),
     moduleTypes = require('./modules.js'),
@@ -15,20 +22,69 @@ const programTypes = require('./program.js'),
     typescriptTypes = require('./typescript.js'),
     primitiveTypes = require('./primitives.js');
 
-// Exports
+// Assemble types
+Object.assign(
+    types,
+    programTypes,
+    moduleTypes,
+    statementTypes,
+    declarationTypes,
+    functionTypes,
+    classTypes,
+    patternTypes,
+    expressionTypes,
+    objectTypes,
+    literalTypes,
+    jsxTypes,
+    typescriptTypes,
+    primitiveTypes
+);
 
-module.exports = {
-    ...programTypes,
-    ...moduleTypes,
-    ...statementTypes,
-    ...declarationTypes,
-    ...functionTypes,
-    ...classTypes,
-    ...patternTypes,
-    ...expressionTypes,
-    ...objectTypes,
-    ...literalTypes,
-    ...jsxTypes,
-    ...typescriptTypes,
-    ...primitiveTypes
-};
+for (const [name, type] of Object.entries(types)) {
+    type.name = name;
+}
+
+/**
+ * Initialize type.
+ * If string provided, looks up type object by name in `types`.
+ * @param {string|Object} type - Type name or type object
+ * @returns {Object} - Type object
+ */
+function initType(type) {
+    if (typeof type === 'string') {
+        assert(types[type], `Type not found: ${type}`);
+        type = types[type];
+    }
+
+    if (!type.isInitialized) {
+        type.isInitialized = true;
+        getTypeName(type);
+        type.init();
+        assert(typeof type.name === 'string', 'No type name');
+        assert(isPositiveInteger(type.length), `${type.name} type has no length`);
+        assert(isPositiveInteger(type.align), `${type.name} type has no align`);
+        types[type.name] = type;
+    }
+
+    return type;
+}
+
+/**
+ * Get name of type. Calls `type.getName()` to get name.
+ * @param {string|Object} type - Type name or type object
+ * @returns {string} - Type name
+ */
+function getTypeName(type) {
+    if (typeof type === 'string') return type;
+    if (!type.name) type.name = type.getName();
+    return type.name;
+}
+
+/**
+ * Check if input is a positive integer.
+ * @param {*} num - Input
+ * @returns {boolean} - `true` if input is a positive integer
+ */
+function isPositiveInteger(num) {
+    return typeof num === 'number' && num !== 0 && !isNaN(num) && num % 1 === 0;
+}

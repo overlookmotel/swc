819ed01b607fcebb6e45b677e491a160abcf9c9b
overlookmotel <theoverlookmotel@gmail.com>
2022-05-22 13:14:30 +0100

    Move constants to separate file [refactor]

---
+++ deserialize/constants.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Modules
+const assert = require('assert');
+
+// Exports
+
+const SERIALIZE_INITIAL_BUFFER_SIZE = 8 * 1024, // 8 KiB
+    SCRATCH_INITIAL_BUFFER_SIZE = 8 * 1024; // 8 KiB
+
+// Buffers must be sized in chunks of 8 bytes, to allow access as Float64Array
+assert(SERIALIZE_INITIAL_BUFFER_SIZE % 8 === 0);
+assert(SCRATCH_INITIAL_BUFFER_SIZE % 8 === 0);
+
+module.exports = {
+    SERIALIZE_INITIAL_BUFFER_SIZE,
+    SCRATCH_INITIAL_BUFFER_SIZE
+};
